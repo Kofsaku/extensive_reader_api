c4844e5e97f7b3862df4303484bea6b487f1ac11
Kofsaku <k_tsubata@azoop.co.jp>
2024-04-10 15:22:03 +0900

    add generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/hello', to: 'hello#index'
+      resources :text_generators, only: [:create]
     end
   end
 end
